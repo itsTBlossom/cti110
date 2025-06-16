183f10f5080bb3842c56fe3a9635a6280c4361b6
itsTBlossom <tblossom.gaming@gmail.com>
2025-06-16 11:10:49 -0400

    Add files via upload

---
+++ P1LAB1_BlossomNatisha.py
@@ -0,0 +1,10 @@
+#Natisha Blossom
+#June 16, 2025
+#P1LAB1
+#This is a program to record input from a user to welcome them to CTI-110
+
+print("Enter your first name:")
+first_name = input()
+print("Enter your last name:")
+last_name = input()
+print("Hello, " + first_name, last_name + "! Welcome to CTI-110")
